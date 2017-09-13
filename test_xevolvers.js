46dfb272c89d767c4ecfa72527bbf8d4af6cbae1
alexjaps <31873595+alexjaps@users.noreply.github.com>
2017-09-13 08:33:42 -0600

    Test Two

--- test_xevolvers.js
+++ test_xevolvers.js
@@ -26,7 +26,8 @@ function unsetBinaries(decimalNumber = 16)
   return cont;
 }
 
-console.log(unsetBinaries(12547));
+//console.log(unsetBinaries(656535)); // return 0
+//console.log(unsetBinaries(12547)); // return 11
 
 /* Code a program that prints the numbers from 1 to 100. 
 But for multiples of 3 prints "Fizz" instead of the number, 
@@ -35,27 +36,39 @@ for multiples of both three and five prints “FizzBuzz”
 and code it considering extensibility (meaning you can easily add more multipliers). 
 */
 
-function checkMultiplesOf(multiple1 = 3, multiple2 = 5) //DEFAULT MULTIPLES SEARCHED ARE 3 and 5
+function checkMultiplesOf()
   {
+    var numbers = [];
+    var multiples = [[3,5,"Fizzbuzz"],[3,"Fizz"]];
+      
+    //multiples.push([2,4,"Fizzbuzz"]); // structure to add new multiples of two values multiples.push([val1,val2, "message"])
+      
+    //multiples.push([4,"Fizz"]); // structure to add new multiplies of one value : multiples.push([val,"message"])
+    multiples.push([5,"buzz"]);
     for(var x=1; x<=100; x++)
     {
-      if(x%multiple1 == 0 && x%multiple2 == 0)
-       console.log("FizzBuzz ")
-      else
-      {
-        if(x%multiple1 == 0)
-          console.log("Fizz ")
-        else
-        {
-           if(x%multiple2 == 0)
-            console.log("Buzz ")
-           else
-            console.log(x)
+        var printed = false;
+        for(y in multiples){
+            if(multiples[y].length == 3){
+                if(x%multiples[y][0]==0 && x%multiples[y][1]==0){
+                    numbers.push(multiples[y][2]);
+                    printed = true;
+                    break;
+                }
+            }else if(multiples[y].length==2){
+                if(x%multiples[y][0]==0){
+                    numbers.push(multiples[y][1]);
+                    printed = true;
+                    break;
+                }
+            }
+          
         }
-      }
+        if(!printed) numbers.push(x);
     }
+    return numbers;
   }
-  checkMultiplesOf(3,7);
+  //console.log(checkMultiplesOf());
 
 
  /*
@@ -63,33 +76,35 @@ function checkMultiplesOf(multiple1 = 3, multiple2 = 5) //DEFAULT MULTIPLES SEAR
    and returns the age of the user, you need to throw an exception
    in case of incorrect inputs (negative numbers, not numbers, 
    ages outside the human lifespan, etc.) 
- */
-  function whatsMyAge(birthdate)
-  {
-    var today = new Date();
-    var yyyy = today.getFullYear();
-    var averageAge = 80;
-    var age = yyyy - birthdate;
-
-    if(age>=averageAge)
-    {
-      alert("THIS DATE EXCEEDS AVERAGE LIFESPAN")
-      return;
-    }
-    if(birthdate < 0)
-    {
-      alert("BIRTHDATE MUST NOT BE LESS OR EQUAL TO 0")
-      return;
-    }
-    if(isNaN(birthdate))
-    {
-      alert("BIRTHDATE IS NOT NUMERIC")
-      return;
-    }
-
-    return age;
-  }
-  console.log(whatsMyAge(1996))
+     */
+      function whatsMyAge(birthdate)
+      {
+        var today = new Date();
+        var yyyy = today.getFullYear();
+        var averageAge = 80;
+        var age = yyyy - birthdate;
+          
+        if(birthdate < 0)
+        {
+          return("BIRTHDATE MUST NOT BE LESS OR EQUAL TO 0");
+        }
+          
+        if(age >=averageAge)
+        {
+          return("THIS DATE EXCEEDS AVERAGE LIFESPAN");
+        }
+       
+        if(isNaN(birthdate))
+        {
+          return("BIRTHDATE IS NOT NUMERIC");
+        }
+        
+        if(birthdate >= yyyy){
+            return("BIRTHDATE MUST BE LESS TO CURRENT YEAR");
+        }
+        return "You have "+ age +" years old";
+      }
+      //console.log(whatsMyAge(1996))
 
 
 /*
@@ -97,33 +112,45 @@ Code a function that receives 2 sorted arrays as parameter
 and merge them into a 3rd sorted array do not use any built-in 
 sort method since the input arrays are already sorted
 */
-  function orderArray(arr1 = [4,5,7,9,10,34], arr2 = [1,3,6,8,32,33])
+function orderArray(arr1 = [4,5,7,9,10,34], arr2 = [1,3,6,8,32,33])
   {
-
-    function sort(a)
-    {
-      var aux;
-      do {
-          aux = false;
-          for (var i=0; i < a.length-1; i++) {
-              if (a[i] > a[i+1]) {
-                  var temp = a[i];
-                  a[i] = a[i+1];
-                  a[i+1] = temp;
-                  aux = true;
-              }
+      var auxarray = [];
+      var sortarray = [];
+      var added = false;
+      if(arr1[0] < arr2[0]){
+          return newArray(arr1,arr2);
+      }else{
+          return newArray(arr2,arr1);
+      }
+      
+      function newArray(array1,array2){
+          for(x in array1){
+              auxarray.push(array1[x]);
           }
-      } while (aux);
-    }
-    var arr3 = [];
-
-    for(x=0; x<arr1.length; x++){
-      arr3.push(arr1[x]);
-    }
-    for(x=0; x<arr2.length; x++){
-      arr3.push(arr2[x]);
-    }
-    sort(arr3);
-    return arr3;
+          for(y in array2){
+              auxarray.push(array2[y]);
+          }
+          var increment = auxarray.length / 2;
+          while (increment > 0) {
+            for (i = increment; i < auxarray.length; i++) {
+                var j = i;
+                var temp = auxarray[i];
+                while (j >= increment && auxarray[j-increment] > temp) {
+                    auxarray[j] = auxarray[j-increment];
+                    j = j - increment;
+                }
+                auxarray[j] = temp;
+            }
+           if (increment == 2) {
+                increment = 1;
+            } else {
+                increment = parseInt(increment*5 / 11);
+            }
+        }
+          for(x in auxarray){ if(auxarray[x]!= undefined){ sortarray.push(auxarray[x]); } }
+          return sortarray;
+      }  
+      
   }
-  console.log(orderArray());
+
+//console.log(orderArray());
